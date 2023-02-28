5e07211c8b4c6d16f72273981297b9b266e014dc
thamizh-v@abfrl.adityabirla.com <krithika.t@codingmart.com>
2023-02-28 18:07:02 +0530

    recent changes

--- controller/course.controller.ts
+++ controller/course.controller.ts
@@ -1,78 +1,88 @@
-import {PrismaClient} from "@prisma/client";
-import Koa from 'koa'
-const prisma = new PrismaClient()
-let addCourse =async (ctx:Koa.Context) => {
-    try {
-        const course = ctx.request.body;
-        const isEmpty = !Object.values(course).every(x => x !== null && x !== '');
-        if(isEmpty)
-       {ctx.status=404;
-        return   ctx.body={
-        message:"all details are required to add the course!",
-        status:false
-
-       } 
-        }
-        const user_id =ctx.params.id ;
-        const {id} = await prisma.teacher.findFirst({where:{userId:user_id},select:{id:true}})
-         console.log(id)
-            course.slug = ctx.request.body.course_name.toLowerCase().replaceAll(" ", "-");
-            course.teacherId= id
-            course.schedule_date = new Date(course.schedule_date)
-            console.log(course);
-              let courseData = await prisma.course.create({data:course})
-            return  ctx.body={ message: "Course created",status:true ,data:courseData};
-    } catch (error) {
-      ctx.status=400
-      ctx.body={ message: error.message,status:false };
-    }  
-}
-let fetchCourseS =async (ctx:Koa.Context) => {
+import { PrismaClient } from "@prisma/client";
+import Koa from "koa";
+const prisma = new PrismaClient();
+let addCourse = async (ctx: Koa.Context) => {
   try {
-    const courseData = await prisma.course.findMany({
-      // Returns all courses
-      include: {
-        Teacher:{}
-      },
-    })
-    
-    ctx.status=200;
-   return ctx.body={
-      data:courseData,
-      status:true
+    const course = ctx.request.body;
+    const isEmpty = !Object.values(course).every(
+      (attr) => attr !== null && attr !== ""
+    );
+    if (isEmpty) {
+      ctx.status = 404;
+      return (ctx.body = {
+        message: "all details are required to add the course!",
+        status: false,
+      });
     }
+    const user_id = ctx.params.id;
+    const { id } = await prisma.teacher.findFirst({
+      where: { userId: user_id },
+      select: { id: true },
+    });
+    console.log(id);
+    course.slug = ctx.request.body.course_name
+      .toLowerCase()
+      .replaceAll(" ", "-");
+    course.teacherId = id;
+    course.schedule_date = new Date(course.schedule_date);
+    console.log(course);
+    let courseData = await prisma.course.create({ data: course });
+    return (ctx.body = {
+      message: "Course created",
+      status: true,
+      data: courseData,
+    });
   } catch (error) {
-    ctx.status=400
-    console.log(error.message)
+    ctx.status = 400;
+    ctx.body = { message: error.message, status: false };
   }
-}
-let fetchCourseT =async (ctx:Koa.Context) => {
+};
+let fetchCourseS = async (ctx: Koa.Context) => {
   try {
-    const user_id =ctx.params.id ;
-    const courses = (await prisma.teacher.findFirst({
-      where: {
-        userId: user_id
+    // Returns all courses
+    const courseData = await prisma.course.findMany({
+      include: {
+        Teacher: {},
       },
+    });
+
+    ctx.status = 200;
+    return (ctx.body = {
+      data: courseData,
+      status: true,
+    });
+  } catch (error) {
+    ctx.status = 400;
+    console.log(error.message);
+  }
+};
+let fetchCourseT = async (ctx: Koa.Context) => {
+  try {
+    const user_id = ctx.params.id;
+    const courses = (
+      await prisma.teacher.findFirst({
+        where: {
+          userId: user_id,
+        },
         include: {
-          courses: true
-        }
-    })).courses;
+          courses: true,
+        },
+      })
+    ).courses;
 
     console.log(courses);
 
-
-    
-    ctx.status=200;
-   return ctx.body={
-      data:courses,
-      status:true
-    }
+    ctx.status = 200;
+    return (ctx.body = {
+      data: courses,
+      status: true,
+    });
   } catch (error) {
-    ctx.status=400
-    ctx.body={
-      error:error
-    }
-    console.log(error.message)
+    ctx.status = 400;
+    ctx.body = {
+      error: error,
+    };
+    console.log(error.message);
   }
-}
-export{addCourse,fetchCourseS,fetchCourseT}
+};
+export { addCourse, fetchCourseS, fetchCourseT };
--- index.ts
+++ index.ts
@@ -6,20 +6,16 @@ import Router from "koa-router";
 import {config} from "./config/config"
 import { createServer } from "http";
 import { Server } from "socket.io";
-//import * as socketio from "socket.io";
-//import {Socket} from './socketManager'
 const router = new Router()
 const app = new Koa()
 const PORT = config.port;
 let httpServer = createServer(app.callback());
 const socketIO = new Server(httpServer, {
   cors: {
-      origin: "http://localhost:5173"
+      origin: "*"
   }
 });
-//const io = require("socket.io")(httpServer);
 app.use (bodyParser({
- // formidable:{uploadDir: './uploads'},
   multipart: true,
   urlencoded: true
 }));
@@ -42,24 +38,9 @@ app.use(logger())
 require("./routes/auth.routes")(app);
 require("./routes/course.routes")(app);
 require("./routes/class.routes")(app);
-//require("./routes/link.routes")(app);
-
-//io.on("connection",Socket)
-//declare function require(name:string);
-// const user = require("./middleware/index");
-// router.get("/",async (ctx)=>{
-//   try {
-//     ctx.body ={
-//       status:"success"
-//     }
-//   } catch (error) {
-//     console.log(error)
-//   }
-// });
-//app.use(router.routes())
 const server =httpServer
   .listen(PORT, async()=>{
-    console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT} 😇`)
   })
    .on ("error",err=>{
     console.error(err)
